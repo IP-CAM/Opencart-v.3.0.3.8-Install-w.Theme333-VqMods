704d9b2ff8b64c23997de18a38ad83974b6362e9
jrcook416 <kc9cqj@gmail.com>
2022-07-26 11:12:26 -0700

    finishing up fixing things.

---
+++ upload/admin/config-admin-3038.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://indyems.iemssupply.website/admin/');
+define('HTTP_CATALOG', 'https://indyems.iemssupply.website/');
+// HTTPS
+define('HTTPS_SERVER', 'https://indyems.iemssupply.website/admin/');
+define('HTTPS_CATALOG', 'https://indyems.iemssupply.website/');
+// DIR
+define('DIR_APPLICATION', '/home/penf0zhrijfn/public_html/opencart/upload/admin/');
+define('DIR_SYSTEM', '/home/penf0zhrijfn/public_html/opencart/upload/system/');
+define('DIR_IMAGE', '/home/penf0zhrijfn/public_html/opencart/upload/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storagebct3ysi4lcxo/');
+define('DIR_CATALOG', '/home/penf0zhrijfn/public_html/opencart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'wombat-vengeance-oc');
+define('DB_PASSWORD', ');N#;?ucSvWW');
+define('DB_DATABASE', 'iems_openshop');
+define('DB_PREFIX', 'oc2_');
+define('DB_PORT', '3306');
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
